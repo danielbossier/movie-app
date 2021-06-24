a4c646885f4c8e33e88d8063ac03cf9afae0bf0c
danielbossier <dbossier21@gmail.com>
2021-06-23 23:20:20 -0700

    added gender, age to actor, added director, english to movie

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-actor = Actor.new({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
-actor.save
+# actor = Actor.new({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
+# actor.save
 
-actor = Actor.new({ first_name: "Kit", last_name: "Harrington", known_for: "Game of Thrones" })
-actor.save
+# actor = Actor.new({ first_name: "Kit", last_name: "Harrington", known_for: "Game of Thrones" })
+# actor.save
 
-movie = Movie.new({ title: "Goodfellas", year: 1990, plot: "All Henry's life he wanted to be a ganster. Too bad it involves a lot of burried bodies and cocaine." })
-movie.save
+# movie = Movie.new({ title: "Goodfellas", year: 1990, plot: "All Henry's life he wanted to be a ganster. Too bad it involves a lot of burried bodies and cocaine." })
+# movie.save
 
-movie = Movie.new({ title: "Jaws", year: 1975, plot: "They could have used a bigger boat." })
-movie.save
+# movie = Movie.new({ title: "Jaws", year: 1975, plot: "They could have used a bigger boat." })
+# movie.save
