4c40edb97939d67425a431a4dfcaea6195d89718
danielbossier <dbossier21@gmail.com>
2021-07-01 20:28:20 -0700

    added user model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
   get "/actors/:id" => "actors#show"
   patch "/actors/:id" => "actors#update"
   delete "/actors/:id" => "actors#destroy"
+
+  post "/users" => "users#create"
 end
