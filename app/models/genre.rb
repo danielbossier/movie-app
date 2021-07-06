991075459c0292629df8e5d4f6ed908e0c112c7f
danielbossier <dbossier21@gmail.com>
2021-07-06 15:42:20 -0700

    added movie serializer

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,4 @@
 class Genre < ApplicationRecord
+  has many :movie_genres
+  has many :movies, through: :movie_genres
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 
   validates :title, length: { minimum: 2 }
   validates :plot, presence: true
