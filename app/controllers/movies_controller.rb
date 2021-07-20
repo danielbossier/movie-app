e7767b887c302b7054e7689d873f1bdac04df52b
danielbossier <dbossier21@gmail.com>
2021-07-19 22:16:24 -0700

    fixed create movie method from front end

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,11 +15,8 @@ class MoviesController < ApplicationController
       director: params["director"],
       english: params["english"],
     )
-    if movie.save
-      render json: movie
-    else
-      render json: { errors: movie.errors.full_messages }, status: :unprocessable_entity
-    end
+    movie.save
+    render json: movie.as_json
   end
 
   def show
