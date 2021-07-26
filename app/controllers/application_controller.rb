f5b9412c8efcaeb0005ff3ea53ab4d9fdb7bc10e
danielbossier <dbossier21@gmail.com>
2021-07-26 14:59:56 -0700

    fixed login issue, reset DB

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
+  # protect_from_forgery with: :null_session
+
   def current_user
-    # return User.first
     auth_headers = request.headers["Authorization"]
     if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
       token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# actor = Actor.new({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
-# actor.save
+actor = Actor.new({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
+actor.save
 
-# actor = Actor.new({ first_name: "Kit", last_name: "Harrington", known_for: "Game of Thrones" })
-# actor.save
+actor = Actor.new({ first_name: "Kit", last_name: "Harrington", known_for: "Game of Thrones" })
+actor.save
 
-# movie = Movie.new({ title: "Goodfellas", year: 1990, plot: "All Henry's life he wanted to be a ganster. Too bad it involves a lot of burried bodies and cocaine." })
-# movie.save
+movie = Movie.new({ title: "Goodfellas", year: 1990, plot: "All Henry's life he wanted to be a ganster. Too bad it involves a lot of burried bodies and cocaine.", director: "Martin Scorsese" })
+movie.save
 
-# movie = Movie.new({ title: "Jaws", year: 1975, plot: "They could have used a bigger boat." })
-# movie.save
+movie = Movie.new({ title: "Jaws", year: 1975, plot: "They could have used a bigger boat.", director: "Steven Spielberg" })
+movie.save
 
-# genre = Genre.new(name: "drama")
-# genre.save
+movie = Movie.new({ title: "Dr Strange", year: 2016, plot: "The Doctor is in.", director: "Scott Derrickson" })
+movie.save
 
-# genre = Genre.new(name: "western")
-# genre.save
+movie = Movie.new({ title: "Pulp Fiction", year: 1994, plot: "Jules Winnfield eats a tasty burger, sips a sprite, and feels the hand of God.", director: "Quentin Tarantino" })
+movie.save
 
-# genre = Genre.new(name: "blockbuster")
-# # genre.save
+movie = Movie.new({ title: "Brimstone", year: 2016, plot: "John Snow. In the desert. John Sand.", director: "Martin Koolhoven" })
+movie.save
 
-# moviegenre = MovieGenre.new(movie_id: 5, genre_id: 2)
-# moviegenre.save
+genre = Genre.new(name: "drama")
+genre.save
 
-# moviegenre = MovieGenre.new(movie_id: 2, genre_id: 3)
-# moviegenre.save
+genre = Genre.new(name: "western")
+genre.save
 
-# moviegenre = MovieGenre.new(movie_id: 1, genre_id: 1)
-# moviegenre.save
+genre = Genre.new(name: "blockbuster")
+genre.save
+
+moviegenre = MovieGenre.new(movie_id: 5, genre_id: 2)
+moviegenre.save
+
+moviegenre = MovieGenre.new(movie_id: 2, genre_id: 3)
+moviegenre.save
+
+moviegenre = MovieGenre.new(movie_id: 1, genre_id: 1)
+moviegenre.save
